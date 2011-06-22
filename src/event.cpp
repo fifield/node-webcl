eddc5af9dce38746fe6daff6f7b8578c8bf169c3
fifield <fifield@colorado.edu>
2011-06-22 16:22:16 -0600

    add more of event api

--- src/event.cpp
+++ src/event.cpp
@@ -1,5 +1,7 @@
 
 #include "event.h"
+#include "context.h"
+#include "commandqueue.h"
 
 #include <iostream>
 
@@ -39,13 +41,81 @@ Event::~Event()
 Handle<Value> Event::getEventInfo(const Arguments& args)
 {
     HandleScope scope;
-    return ThrowException(Exception::Error(String::New("getEventInfo unimplemented")));
+    Event *e = ObjectWrap::Unwrap<Event>(args.This());
+    cl_event_info param_name = args[0]->NumberValue();
+    size_t param_value_size_ret = 0;
+    char param_value[1024];
+
+    cl_int ret = EventWrapper::eventInfoHelper(e->getEventWrapper(),
+					       param_name,
+					       sizeof(param_value),
+					       param_value,
+					       &param_value_size_ret);
+
+    if (ret != CL_SUCCESS) {
+	WEBCL_COND_RETURN_THROW(CL_INVALID_VALUE);
+	WEBCL_COND_RETURN_THROW(CL_INVALID_EVENT);
+	WEBCL_COND_RETURN_THROW(CL_OUT_OF_RESOURCES);
+	WEBCL_COND_RETURN_THROW(CL_OUT_OF_HOST_MEMORY);
+	return ThrowException(Exception::Error(String::New("UNKNOWN ERROR")));
+    }
+
+    switch (param_name) {
+    case CL_EVENT_CONTEXT:{
+	cl_context ctx = *((cl_context*)param_value);
+	ContextWrapper *cw = new ContextWrapper(ctx);
+	return scope.Close(CLContext::New(cw)->handle_);
+    }
+    case CL_EVENT_COMMAND_QUEUE:{
+	cl_command_queue q = *((cl_command_queue*)param_value);
+	CommandQueueWrapper *qw = new CommandQueueWrapper(q);
+	return scope.Close(CommandQueue::New(qw)->handle_);
+    }
+    case CL_EVENT_REFERENCE_COUNT:
+	return scope.Close(Number::New(*(cl_uint*)param_value));
+    case CL_EVENT_COMMAND_TYPE:
+    case CL_EVENT_COMMAND_EXECUTION_STATUS:
+	return scope.Close(Number::New(*(cl_int*)param_value));
+    default:
+	return ThrowException(Exception::Error(String::New("UNKNOWN param_name")));
+    }
+ 
 }
 
 /* static  */
 Handle<Value> Event::getEventProfilingInfo(const Arguments& args)
 {
     HandleScope scope;
+    Event *e = ObjectWrap::Unwrap<Event>(args.This());
+    cl_event_info param_name = args[0]->NumberValue();
+    size_t param_value_size_ret = 0;
+    char param_value[1024];
+
+    cl_int ret = EventWrapper::eventProfilingInfoHelper(e->getEventWrapper(),
+							param_name,
+							sizeof(param_value),
+							param_value,
+							&param_value_size_ret);
+
+    if (ret != CL_SUCCESS) {
+	WEBCL_COND_RETURN_THROW(CL_INVALID_VALUE);
+	WEBCL_COND_RETURN_THROW(CL_PROFILING_INFO_NOT_AVAILABLE);
+	WEBCL_COND_RETURN_THROW(CL_INVALID_EVENT);
+	WEBCL_COND_RETURN_THROW(CL_OUT_OF_RESOURCES);
+	WEBCL_COND_RETURN_THROW(CL_OUT_OF_HOST_MEMORY);
+	return ThrowException(Exception::Error(String::New("UNKNOWN ERROR")));
+    }
+
+    switch (param_name) {
+    case CL_PROFILING_COMMAND_QUEUED:
+    case CL_PROFILING_COMMAND_SUBMIT:
+    case CL_PROFILING_COMMAND_START:
+    case CL_PROFILING_COMMAND_END:
+	return scope.Close(Number::New(*(cl_ulong*)param_value));
+    default:
+	return ThrowException(Exception::Error(String::New("UNKNOWN param_name")));
+    }
+ 
     return ThrowException(Exception::Error(String::New("getEventProfilingInfo unimplemented")));
 }
 
@@ -53,7 +123,20 @@ Handle<Value> Event::getEventProfilingInfo(const Arguments& args)
 Handle<Value> Event::setUserEventStatus(const Arguments& args)
 {
     HandleScope scope;
-    return ThrowException(Exception::Error(String::New("setUserEventStatus unimplemented")));
+    Event *e = ObjectWrap::Unwrap<Event>(args.This());
+
+    cl_int ret = e->getEventWrapper()->setUserEventStatus((cl_int)args[0]->NumberValue());
+    
+    if (ret != CL_SUCCESS) {
+	WEBCL_COND_RETURN_THROW(CL_INVALID_EVENT);
+	WEBCL_COND_RETURN_THROW(CL_INVALID_VALUE);
+	WEBCL_COND_RETURN_THROW(CL_INVALID_OPERATION);
+	WEBCL_COND_RETURN_THROW(CL_OUT_OF_RESOURCES);
+	WEBCL_COND_RETURN_THROW(CL_OUT_OF_HOST_MEMORY);
+	return ThrowException(Exception::Error(String::New("UNKNOWN ERROR")));
+    }
+
+    return Undefined();
 }
 
 /* static  */
